4d26485c82db88c7aaabf14b44bc16dfb5edd57b
Vidhan27 <117517355+Vidhan27@users.noreply.github.com>
2023-01-05 21:53:33 +0530

    Initial commit

---
+++ Layout/body.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div id="body">
+      <Nuxt />
+    </div>
+    <footer>
+      <div class="text center">
+        <span>Made by Vidhan , Ayush , Krish S , Krish P , Dhruvraj </span>
